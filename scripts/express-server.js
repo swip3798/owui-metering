09824cd142302bd86b20f6964002193fea34a79b
Christian Schweigel <schweigel@pm.me>
2025-06-26 07:59:20 +0200

    Add environment variable check and logging on startup

--- scripts/express-server.js
+++ scripts/express-server.js
@@ -1,5 +1,29 @@
 import { handler } from './build/handler.js';
 import express from 'express';
+import 'dotenv/config';
+
+const checkOrLogEnvVar = (name, envVar, optional) => {
+  if (!envVar) {
+    if (!optional) {
+      console.error(
+        `Environment variable ${name} is not set, but critical for correct functioning, check your deployment!`
+      );
+    } else {
+      console.warn(
+        `Environment variable ${name} is not set! While not critical, make sure your deployment is setup correctly!`
+      );
+    }
+    return false;
+  }
+  return true;
+};
+
+console.info('Check presence of all required environment variables:');
+checkOrLogEnvVar('DB_FILE_NAME', node.env.DB_FILE_NAME, false);
+checkOrLogEnvVar('PASSWORD_HASH', node.env.PASSWORD_HASH, false);
+checkOrLogEnvVar('JWT_SECRET', node.env.JWT_SECRET, false);
+checkOrLogEnvVar('API_KEY', node.env.API_KEY, false);
+checkOrLogEnvVar('OPENROUTER_API_KEY', node.env.OPENROUTER_API_KEY, true);
 
 const app = express();
 
